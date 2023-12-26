562ab42a3327b1cb30ca62f482f47fe8e05278ee
rahul <rahul.baradol.14@gmail.com>
2023-12-27 00:08:56 +0530

    added socket.io and basic judging system is working

--- server.ts
+++ server.ts
@@ -2,9 +2,11 @@ import http from 'http';
 import express, { Request, Response } from "express";
 import { Server as SocketIOServer, Socket } from 'socket.io';
 import cors from "cors";
+import { Kafka } from 'kafkajs';
 import connectDB from "./middleware/connectdb";
-import profile from './models/profile'
-import systemData from './models/systemData'
+import profile from './models/profile';
+import systemData from './models/systemData';
+import submissionSchema from './models/submission';
 
 const jsonwebtoken = require('jsonwebtoken')
 
@@ -14,6 +16,13 @@ const PORT = process.env.PORT || 3002;
 
 const app = express();
 const server = http.createServer(app);
+
+const kafka = new Kafka({
+   "clientId": "submissionProducer",
+   "brokers": [`${process.env.BROKER_URL}`]
+});   
+
+const producer = kafka.producer();
 
 const io = new SocketIOServer(server, {
    cors: {
@@ -34,10 +43,33 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 io.on('connection', async (socket: Socket) => {
-   await connectDB();
+   socket.on('submissionStatus', async (submissionId: any) => {
+      try {
+         const dbSubmission = await submissionSchema.findOne({
+            submissionId: submissionId
+         });
 
-   // Handle incoming messages from clients
-   socket.on('message', async (message: string) => {
+         if (dbSubmission) {
+            const result = {
+               submissionId: submissionId,
+               submissionStatus: dbSubmission.status
+            }
+            
+            socket.emit('submissionStatus', result);
+         } else {
+            const result = {
+               submissionId: submissionId,
+               submissionStatus: "NA"
+            }
+            socket.emit('submissionStatus', result);           
+         }
+      } catch (error) {
+         console.log("Error occurred in RCE-JUDGE");
+         console.log(error);
+      }
+   })
+
+   socket.on('submitCode', async (message: string) => {
       try {
          const sentData = JSON.parse(message);
          const sentUserDetails = jsonwebtoken.verify(sentData.jwt, process.env.JWT_KEY);
@@ -46,20 +78,43 @@ io.on('connection', async (socket: Socket) => {
          const dbSystemData = await systemData.findOne({ title: "submissions" });
 
          if (!dbUserDetails) {
-            socket.emit('message', "Profile doesn't exist.");
+            socket.emit('submitCode', "Profile doesn't exist.");
             return;
          }
 
          if (sentUserDetails.sessionKey !== dbUserDetails.sessionKey) {
-            socket.emit('message', "Session expired.")
+            socket.emit('submitCode', "Session expired.")
             return;
          }
 
-         const submissionID = dbSystemData.nextSubmissionId;
+         const submissionId = dbSystemData.nextSubmissionId;
 
+         const date = new Date();
+
+         const judgeData = {
+            userEmail: sentUserDetails.email,
+            problemId: 1,
+            submissionId: submissionId,
+            code: sentData.code ? sentData.code : "",
+            time: date
+         }
+         
+         await producer.send({
+            "topic": `Submission`,
+            "messages": [
+               {
+                  "value": JSON.stringify(judgeData),
+                  "partition": sentData.lang
+               }
+            ]
+         })
+
+         socket.emit('submissionStatus', {
+            submissionId: submissionId,
+            submissionStatus: "NA"
+         });
       } catch (error) {
-         console.log("Error occurred");
-         console.log(error);
+         socket.emit('submitCode', "FORB");
       }
    });
 
@@ -69,6 +124,13 @@ io.on('connection', async (socket: Socket) => {
    });
 });
 
-server.listen(PORT, () => {
-   console.log(`Judge Server is running on port ${PORT}`);
+server.listen(PORT, async () => {
+   try {
+      console.log(`Judge Server is running on port ${PORT}`);
+      await connectDB();
+      await producer.connect();
+   } catch (error)  {
+      console.log("Error starting the server.");
+      console.log(error);
+   }
 });
